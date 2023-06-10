a1192a3d3de83144cbb49a1b8386c895e338142e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-06-10 10:01:24 +0900

    vue-core-video-player埋め込み（表示エラー）

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  plugins: [
+    {
+      src: '@/plugins/plugin',
+      mode: 'client',
+    },
+  ],
 })
--- pages/video.vue
+++ pages/video.vue
@@ -5,36 +5,17 @@
     controls
     width="500"
   ></video>
-  <Vue3CanvasVideoPlayer
-    :src="videoPath"
-    :muted="true"
-    :autoplay="true"
-    :range="[10, 20]"
-    :fps="30"
-    :bbox="{
-      data: {
-        100: [0, 200, 100, 400],
-        101: [10, 210, 110, 410],
-        102: [20, 220, 120, 420],
-      },
-      borderSize: 1,
-      borderColor: 'rgba(255, 0, 0, 0.5)',
-      fillColor: 'rgba(0, 0, 255, 0.5)',
-    }"
-    :type="'contain'"
-    :message-time="1000"
-    :preview="true"
-    :dark-mode="true"
-  />
+  <div>
+    <div class="player-container">
+      <vue-core-video-player
+        src="../assets/videos/mov_hts-samp003.mp4"
+      ></vue-core-video-player>
+    </div>
+  </div>
 </template>
 
 <script>
-import Vue3CanvasVideoPlayer from 'vue3-canvas-video-player'
-import 'vue3-canvas-video-player/dist/style.css'
 export default {
-  components: {
-    Vue3CanvasVideoPlayer,
-  },
   layout: 'video',
   data() {
     return {
