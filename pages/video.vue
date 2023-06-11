bc7c185a047ddf4719b758f7bd644fc1e6f19d7f
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-06-12 08:50:38 +0900

    ビデオのアップロードとプレビュー（ライブラリなしでできた！）

--- pages/video.vue
+++ pages/video.vue
@@ -1,20 +1,53 @@
 <template>
   <h1>This is video Page</h1>
-  <video
-    src="../assets/videos/mov_hts-samp003.mp4"
-    controls
-    width="500"
-  ></video>
-  <div></div>
+  <div class="file-input">
+    <!-- ファイル入力 -->
+    <input type="file" accept="video/mp4" @change="fileSelect" />
+    <!-- プレビューとして表示 -->
+    <video controls ref="previewVideo">
+      <source :src="src" type="video/mp4" />
+    </video>
+  </div>
 </template>
 
-<script>
-export default {
-  layout: 'video',
-  data() {
-    return {
-      videoPath: '../assets/videos/mov_hts-samp003.mp4',
+<script setup>
+import { ref } from 'vue'
+const src = ref()
+const previewVideo = ref()
+
+// ファイルを読みんでDataURL(Base64文字列)を返す
+const readFileAsDataURL = (file) => {
+  console.log('file!', file)
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.onload = (evt) => {
+      resolve(evt.target.result)
     }
-  },
+    reader.onerror = reject
+    reader.readAsDataURL(file)
+  })
+}
+// ファイル選択
+const fileSelect = async (e) => {
+  console.log('ファイルチェンジ！', e)
+  const file = e.target.files[0]
+  if (!file || !file.type.match('video/*')) {
+    return
+  }
+  const data = await readFileAsDataURL(file)
+  // 選択された動画を見えるようにする。
+  src.value = data
+  console.log('src!', src.value)
+  // ロードする
+  previewVideo.value.load()
 }
 </script>
+
+<style>
+.file-input {
+  width: 1000px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+</style>
