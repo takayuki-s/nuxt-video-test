26303028ab5b35445c326d4155db9066b9094c3b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-06-14 07:11:29 +0900

    不要コード削除

--- pages/video.vue
+++ pages/video.vue
@@ -1,9 +1,7 @@
 <template>
   <h1>This is video Page</h1>
   <div class="file-input">
-    <!-- ファイル入力 -->
     <input type="file" accept="video/mp4" @change="fileSelect" />
-    <!-- プレビューとして表示 -->
     <video controls ref="previewVideo">
       <source :src="src" type="video/mp4" />
     </video>
@@ -15,17 +13,12 @@ import { ref } from 'vue'
 const src = ref()
 const previewVideo = ref()
 
-// ファイル選択
 const fileSelect = async (e) => {
-  console.log('ファイルチェンジ！', e)
   const file = e.target.files[0]
   if (!file || !file.type.match('video/*')) {
     return
   }
-  // 選択された動画を見えるようにする。
   src.value = URL.createObjectURL(file)
-  console.log('src!', src.value)
-  // ロードする
   previewVideo.value.load()
 }
 </script>
