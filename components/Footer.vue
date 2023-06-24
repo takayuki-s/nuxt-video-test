932e884c653200e26cc55344add85d771e97ce25
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-06-24 10:00:00 +0900

    簡易フッター追加

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="footer">Footer</div>
+</template>
+
+<script></script>
+
+<style>
+.footer {
+  height: 50px;
+  padding: 10px;
+  background-color: lightblue;
+}
+</style>
