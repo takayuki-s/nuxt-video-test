ade5c09fe9aa3f561624f282464c13528fd3ef7f
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2023-06-30 08:17:44 +0900

    nuxt-tailwind読み込み
    npx tailwindcss init

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
